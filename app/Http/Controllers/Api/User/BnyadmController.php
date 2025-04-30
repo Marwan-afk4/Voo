a06f3f3ee681e6d0974d90c16949cc8fd156a24a
Marwan-afk4 <fk413691@gmail.com>
2025-04-30 17:21:54 +0300

    l

--- app/Http/Controllers/Api/User/BnyadmController.php
+++ app/Http/Controllers/Api/User/BnyadmController.php
@@ -30,7 +30,7 @@ class BnyadmController extends Controller
             'front_identity' =>$this->storeBase64Image($request->front_identity, 'user/Front_identity/user_papers'),
             'back_identity' =>$this->storeBase64Image($request->back_identity, 'user/Back/user_papers'),
             'selfi_image' =>$this->storeBase64Image($request->selfi_image, 'user/Selfi/user_papers'),
-            'orgnization_paper' =>$this->storeBase64Image($request->orgnization_papers, 'user/Orgnization_paper/user_papers'),
+            'orgnization_paper' =>$this->storeBase64Image($request->orgnization_paper, 'user/Orgnization_paper/user_papers'),
         ]);
         return response()->json([
             'message' => 'Bnyadm added successfully',
